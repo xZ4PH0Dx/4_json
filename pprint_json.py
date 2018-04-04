1d1ad3788c3e8d4aaafe7a0b2db0e02d35a3a2d8
Z4PH0D <dreamfall3r@gmail.com>
2018-04-04 14:56:50 +0000

    Первая версия

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,27 @@
 import json
+import os
+import sys
+from json.decoder import JSONDecodeError
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as file:
+        return json.loads(file.read())
 
 
 def pretty_print_json(data):
-    pass
+    return json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    pass
+
+    if len(sys.argv) != 2:
+        sys.exit('Вы не указали путь к файлу')
+    filepath = sys.argv[1]
+
+    try:
+        loaded_json = (load_data(filepath))
+    except (OSError, JSONDecodeError):
+        sys.exit('Файл отсутствует или это не json')
+
+    print(pretty_print_json(loaded_json))
